9aa35c762afee2012bd3f742d3810681d33a32ca
LewisTehMinerz <lewisminecraft@hotmail.co.uk>
2016-04-24 10:18:42 +0100

    Update Scriptstaller.js

--- Scriptstaller.js
+++ Scriptstaller.js
@@ -4,8 +4,8 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @updateURL    
-// @downloadURL
+// @updateURL    https://raw.githubusercontent.com/Scriptstaller/Scriptstaller/master/Scriptstaller.js
+// @downloadURL  https://raw.githubusercontent.com/Scriptstaller/Scriptstaller/master/Scriptstaller.js
 // @match        http://*/*
 // @grant        none
 // ==/UserScript==
