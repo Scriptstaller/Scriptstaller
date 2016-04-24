c3d958142a3a95f040865df066fc15712703abfe
LewisTehMinerz <lewisminecraft@hotmail.co.uk>
2016-04-24 10:18:18 +0100

    Create Scriptstaller.js

---
+++ Scriptstaller.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @updateURL    
+// @downloadURL
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
