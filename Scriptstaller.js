1ce359a3321974a366bf866f1ef1615c940e05c7
LewisTehMinerz <lewisminecraft@hotmail.co.uk>
2016-04-24 10:28:31 +0100

    Update Scriptstaller.js

--- Scriptstaller.js
+++ Scriptstaller.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Scriptstaller
 // @namespace    scriptstaller.github.io
-// @version      0.1
+// @version      0.0.0.1
 // @description  Better script installing for Tampermonkey.
 // @author       Scriptstaller | scriptstaller.github.io
 // @updateURL    https://raw.githubusercontent.com/Scriptstaller/Scriptstaller/master/Scriptstaller.js
 // @downloadURL  https://raw.githubusercontent.com/Scriptstaller/Scriptstaller/master/Scriptstaller.js
 // @match        http://*/*
+// @match        https://*/*
 // @grant        none
 // ==/UserScript==
 
